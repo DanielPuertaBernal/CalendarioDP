ec00caa560a945f768ca3eea3a9e62651dc6246d
Daniel <daniel.puerta0522@uco.net.co>
2025-04-12 14:04:03 -0500

    Estructura inicial del proyecto

---
+++ src/main/java/com/calendariodp/CalendarioDpApplication.java
@@ -0,0 +1,13 @@
+package com.calendariodp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarioDpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalendarioDpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/calendariodp/CalendarioDpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calendariodp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarioDpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
